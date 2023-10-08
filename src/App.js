b7e095c169cc860f96168d10e27382dfaea63986
skwasim777 <wasimsk6061@gmail.com>
2023-10-08 19:44:22 +0530

    first app in react js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import SubComponent from './components/SumComponent';
+function App() {
+    return (
+        <SubComponent />
+    );
+}
+export default App;
---
+++ src/components/SumComponent.js
@@ -0,0 +1,7 @@
+import './SubComponent.css';
+function SubComponent() {
+    return (
+        <p className="red">This is a sub component</p>
+    );
+}
+export default SubComponent;
